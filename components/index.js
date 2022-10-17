7d4f4ed6fe81ef181f16253f6cd4d5518e121363
Emmanuel Holonou <holonouemmanuel0@gmail.com>
2022-10-17 11:59:02 +0100

    First commit after watching the tutorial, no errors yet

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as HomeHeader } from "./HomeHeader";
+export { default as FocusedStatusBar } from "./FocusedStatusBar";
+export { default as NftCard } from "./NftCard";
+export { CircleButton, RectButton } from "./Button";
+export { SubInfo, NFTTitle, ImageCmp, EndDate, EthPrice, People } from "./SubInfo";
+export {default as DetailsBid} from "./DetailsBid"
+export {default as DetailsDesc} from "./DetailsDesc"
